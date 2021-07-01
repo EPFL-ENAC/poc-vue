ebbf04377703a61c18d3fbf569d604dbb2bc23a9
David Tang <dev@blueur.com>
2021-07-01 14:40:13 +0200

    users tab

---
+++ src/views/Users.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="user of users" v-bind:key="user">
+        {{ user }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component({
+  components: {},
+})
+export default class Users extends Vue {
+  users: string[] = [];
+
+  created(): void {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then((users) => (this.users = users));
+  }
+}
+</script>
